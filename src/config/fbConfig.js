e217c6ee986336744f36645d737ff517d9815f7b
Cortlan Bainbridge <bainbric@uni.edu>
2018-12-02 13:29:03 -0600

    using env variables instead

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var config = {
-    apiKey: "AIzaSyBj1zvuapHEXxMAXE83qVw77AMejt8mrXI",
-    authDomain: "grocery-list-73afe.firebaseapp.com",
-    databaseURL: "https://grocery-list-73afe.firebaseio.com",
-    projectId: "grocery-list-73afe",
-    storageBucket: "grocery-list-73afe.appspot.com",
-    messagingSenderId: "514960464825"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
